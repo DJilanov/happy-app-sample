382444c8896248fa643d9e0d751b7c22f549ff2e
Dimitar Jilanov <djilanov@gmail.com>
2019-11-28 10:03:57 +0200

    Basic webpage

---
+++ resources/scroll-1574928207976.js
@@ -0,0 +1,41 @@
+(function(window, undefined) {
+
+    /*********************** START STATIC ACCESS METHODS ************************/
+
+    jQuery.extend(jimUtil, {
+        "loadScrollBars": function() {
+            jQuery(".s-9e386aa8-656e-4630-8b82-e78f3b685493 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-4d106711-bd40-480a-b01e-4774e30ed78c .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-6c57dc4e-7cfd-45fe-86ad-bb405ce4cfda .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-4153d8bd-22ea-4b0b-811a-a8cc70a4ceb1 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-13240af0-13c0-4cc7-93d9-902c712104a5 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-87a2045a-eba6-412b-ac80-a3a19fc50a4f .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-5b8ae02e-09b3-4f7e-ba32-e220412eb430 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-70bb128e-46f2-4626-8d71-4815e1caf146 .ui-page").overscroll({ showThumbs:true, direction:'multi', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-afc0c54a-74f9-405a-88ba-cc6550a454bc .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-0753223c-98c8-408f-8d48-c995cf61e5fa .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-c7b4759c-4725-46bd-8f61-101c7f5b601f .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-ae8b2c19-2212-4cfe-b7fc-863a2e478e64 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-a78632eb-fc15-4eb1-a883-94b15ff54b8f .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-95adc887-3e85-4566-95d6-d7b6077c8c85 .ui-page").overscroll({ showThumbs:true, direction:'multi', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-88dfb81f-8382-4755-a7fd-b9c39c41f65a .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-7a4e4b28-6029-44e2-8d58-afc7eaa47916 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-920cd32c-d16d-4fe6-b445-148afcfaf7e5 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-411088f4-eda5-4b42-8a29-3afc48503026 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-76daaa3a-8bf7-4ff7-bdf1-d7d1db9ebc53 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-d04d0931-a344-4d08-8262-df4d56843c80 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-f51250c9-f8db-4dfb-b88c-954024739358 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-8249be74-3a2c-42a4-bd22-27be2244f83a .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-61e9b4b6-e4b8-46d2-84a6-b0e1973c7fa7 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-ba32d0c8-5516-4168-adc0-361480a3b6c2 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-d452fd31-7569-427c-852a-3fa21f65c799 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-43a6f81a-5c90-47ab-8525-1f8a1c7d362a .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-4f20b5af-e352-4f0d-93b2-918dbd0cb7c1 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-8122a80f-497f-4de0-80a4-fe810e03a7f9 .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+            jQuery(".s-45eb01c5-d9da-42a2-a08d-d3137260019b .ui-page").overscroll({ showThumbs:true, direction:'vertical', roundCorners:true, backgroundColor:'black', opacity:'0.5', thickness:'2'});
+         }
+    });
+
+    /*********************** END STATIC ACCESS METHODS ************************/
+
+}) (window);
